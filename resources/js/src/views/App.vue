949c4e6a3c1cb5f7029a632d32fdc4bea29af231
coder866 <coder866@gmail.com>
2021-12-09 02:54:47 +0300

    initial commit

---
+++ resources/js/src/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>CASL MANENOZ</h1>
+        <transition type="animation">
+            <router-view/>
+        </transition>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
